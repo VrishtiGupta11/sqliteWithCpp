6a109c6f873b98a729c37673bde1146eebba364b
Vrishti Gupta <53931644+VrishtiGupta11@users.noreply.github.com>
2021-12-08 10:04:55 +0530

    Update linearSearch.cpp

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -5,13 +5,11 @@
 using namespace std;
 
 string rollNo[10];
-int c=0;
+int c = 0;
 
 static int createDB(const char* s);
 static int createTable(const char* s);
-static int deleteData(const char* s);
 static int insertData(const char* s);
-static int updateData(const char* s);
 static int selectData(const char* s);
 static int callback(void* NotUsed, int argc, char** argv, char** azColName);
 
@@ -19,12 +17,12 @@ void linearSearch(string toSearch) {
 	int i = 0;
 	while (i < 6) {
 		if (rollNo[i] == toSearch) {
-			cout << "Roll no. "<<toSearch<<" found at position " << i + 1;
+			cout << "Roll no. " << toSearch << " found at position " << i + 1 << endl;
 			break;
 		}
 		i++;
 		if (i == 6) {
-			cout << "Roll no. not found";
+			cout << "Roll no. " << toSearch << " not found!\n";
 		}
 	}
 }
@@ -35,11 +33,9 @@ int main()
 
 	createDB(dir);
 	//createTable(dir);
-	// deleteData(dir); // use prior to insert
 	//insertData(dir); // uncomment the deleteData above to avoid duplicates
-	// updateData(dir);
 	selectData(dir);
-	
+
 	string toSearch;
 	cout << "Enter Element to Search: ";
 	cin >> toSearch;
@@ -83,7 +79,7 @@ static int createTable(const char* s)
 		else {
 			cout << "Table created Successfully" << endl;
 		}
-			sqlite3_close(DB);
+		sqlite3_close(DB);
 	}
 	catch (const exception& e)
 	{
@@ -120,45 +116,6 @@ static int insertData(const char* s)
 	return 0;
 }
 
-static int updateData(const char* s)
-{
-	sqlite3* DB;
-	char* messageError;
-
-	string sql("UPDATE GRADES SET GRADE = 'A' WHERE LNAME = 'Cooper'");
-
-	int exit = sqlite3_open(s, &DB);
-	/* An open database, SQL to be evaluated, Callback function, 1st argument to callback, Error msg written here */
-	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
-	if (exit != SQLITE_OK) {
-		cerr << "Error in updateData function." << endl;
-		sqlite3_free(messageError);
-	}
-	else
-		cout << "Records updated Successfully!" << endl;
-
-	return 0;
-}
-
-static int deleteData(const char* s)
-{
-	sqlite3* DB;
-	char* messageError;
-
-	string sql = "DELETE FROM GRADES;";
-
-	int exit = sqlite3_open(s, &DB);
-	/* An open database, SQL to be evaluated, Callback function, 1st argument to callback, Error msg written here */
-	exit = sqlite3_exec(DB, sql.c_str(), callback, NULL, &messageError);
-	if (exit != SQLITE_OK) {
-		cerr << "Error in deleteData function." << endl;
-		sqlite3_free(messageError);
-	}
-	else
-		cout << "Records deleted Successfully!" << endl;
-
-	return 0;
-}
 
 static int selectData(const char* s)
 {
@@ -185,15 +142,6 @@ static int selectData(const char* s)
 /* argc: holds the number of results, argv: holds each value in array, azColName: holds each column returned in array, */
 static int callback(void* NotUsed, int argc, char** argv, char** azColName)
 {
-	/*
-	for (int i = 0; i < argc; i++) {
-		// column name and value
-		cout << azColName[i] << ": " << argv[i] << endl;
-	}
-
-	cout << endl;
-	*/
-	
 	rollNo[c] = argv[0];
 	c++;
 
